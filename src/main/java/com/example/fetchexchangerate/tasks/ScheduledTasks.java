dbf80e14f073a8d1e3f777d644c997df2ed9e75c
Albert <witsalbert@gmail.com>
2024-01-18 16:20:58 +0800

    新增 排程 每日下午六點執行

---
+++ src/main/java/com/example/fetchexchangerate/tasks/ScheduledTasks.java
@@ -0,0 +1,21 @@
+package com.example.fetchexchangerate.tasks;
+
+import com.example.fetchexchangerate.services.ExchangeRateService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    private final ExchangeRateService service;
+
+    public ScheduledTasks(ExchangeRateService service) {
+        this.service = service;
+    }
+
+    @Scheduled(cron = "0 00 18 * * ?")
+    public void fetchDailyExchangeRate() throws JsonProcessingException {
+        service.getExchangeRate();
+    }
+}
