457b8ce642c7f32f313f29465fb35259b932e8f1
Albert <witsalbert@gmail.com>
2024-01-18 15:55:52 +0800

    1.init

---
+++ src/main/java/com/example/fetchexchangerate/FetchExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package com.example.fetchexchangerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchExchangeRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchExchangeRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fetchexchangerate/FetchExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fetchexchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchExchangeRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
